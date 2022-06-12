fa439fbd4044361542090152b4fd9c6af627b06c
David Morais <104798002+SirD007@users.noreply.github.com>
2022-06-11 22:02:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const tag_form = document.getElementById('tag_data');
+const company = tag_form.elements['company'];
+const recipient_name = tag_form.elements['name'];
+const address = tag_form.elements['address'];
+const house_number = tag_form.elements['house_number'];
+const district = tag_form.elements['district'];
+const complement = tag_form.elements['complement'];
+const postal_code = tag_form.elements['postal_code'];
+const city = tag_form.elements['city'];
+const state = tag_form.elements['state'];
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+const states = {
+    "AC": "Acre",
+    "AL": "Alagoas",
+    "AP": "Amapá",
+    "AM": "Amazonas",
+    "BA": "Bahia",
+    "CE": "Ceará",
+    "DF": "Distrito Federal",
+    "ES": "Espírito Santo",
+    "GO": "Goiás",
+    "MA": "Maranhão",
+    "MT": "Mato Grosso",
+    "MS": "Mato Grosso do Sul",
+    "MG": "Minas Gerais",
+    "PA": "Pará",
+    "PB": "Paraíba",
+    "PR": "Paraná",
+    "PE": "Pernambuco",
+    "PI": "Piauí",
+    "RJ": "Rio de Janeiro",
+    "RN": "Rio Grande do Norte",
+    "RS": "Rio Grande do Sul",
+    "RO": "Rondônia",
+    "RR": "Roraima",
+    "SC": "Santa Catarina",
+    "SP": "São Paulo",
+    "SE": "Sergipe",
+    "TO": "Tocantins"
+}
+
+postal_code.addEventListener('blur', (event) => {
+    const value = postal_code.value.replace(/[^0-9]+/, '');
+    const url = `https://viacep.com.br/ws/${value}/json/`;
+    fetch(url)
+        .then( response => response.json())
+        .then( json => {
+        if(json.logradouro) {
+            address.value = json.logradouro;
+            district.value = json.bairro;
+            city.value = json.localidade;
+            state.value = states[json.uf];
+        }
+    });
+})
+
+
+tag_form.addEventListener('submit', (event) => {
+    createCookie("company", company.value, 1);
+    createCookie("recipient_name", recipient_name.value, 1);
+    createCookie("address", address.value, 1);
+    createCookie("house_number", house_number.value, 1);
+    createCookie("district", district.value, 1);
+    createCookie("complement", complement.value, 1);
+    createCookie("postal_code", postal_code.value, 1);
+    createCookie("city", city.value, 1);
+    createCookie("state", state.value, 1);
+});
---
+++ tag_script.js
@@ -0,0 +1,36 @@
+const logo = document.getElementById('logo');
+const recipient_name = document.getElementById('name');
+const address = document.getElementById('address');
+const house_number = document.getElementById('house_number');
+const complement = document.getElementById('complement');
+const postal_code = document.getElementById('postal_code');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+let logos = {
+	"Monstrosol": "assets/logo-monstrosol.png",
+  	"MaskPro": "assets/logo-maskpro.png"
+};
+
+let logo_path = logos[readCookie('company')];
+logo.setAttribute('src', logo_path);
+recipient_name.innerText = readCookie('recipient_name')
+address.innerText = readCookie('address')
+house_number.innerText = readCookie('house_number')
+complement.innerText = readCookie('complement')
+if (complement.innerText === "") {complement.innerText = "-"}
+postal_code.innerText = readCookie('postal_code')
+city.innerText = readCookie('city')
+state.innerText = readCookie('state')
+
